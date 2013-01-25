5633058db44363552b042d93ea18d2451d6f97ae
Joshua MacDonald <jmacd@google.com@code.google.com>
2013-01-25 00:46:48 -0800

    Update copyright notices; scrub a few references to Cord, StringPiece

--- btree.h
+++ btree.h
@@ -1,6 +1,16 @@
-// Copyright 2007 Google Inc. All Rights Reserved.
-// Author: jmacd@google.com (Josh MacDonald)
-// Author: pmattis@google.com (Peter Mattis)
+// Copyright 2013 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 // A btree implementation of the STL set and map interfaces. A btree is both
 // smaller and faster than STL set/map. The red-black tree implementation of
@@ -112,12 +122,12 @@
 namespace btree {
 
 // Inside a btree method, if we just call swap(), it will choose the
-// btree::swap method, which we don't want. And we can't say ::swap because
-// then MSVC won't pickup any std::swap() implementations. We can't just use
-// std::swap() directly because then we don't get the specialization for string
-// and Cord (which are not defined in the std namespace!). So the solution is
-// to have a special swap helper function whose name doesn't collide with other
-// swap functions defined by the btree classes.
+// btree::swap method, which we don't want. And we can't say ::swap
+// because then MSVC won't pickup any std::swap() implementations. We
+// can't just use std::swap() directly because then we don't get the
+// specialization for types outside the std namespace. So the solution
+// is to have a special swap helper function whose name doesn't
+// collide with other swap functions defined by the btree classes.
 template <typename T>
 inline void btree_swap_helper(T &a, T &b) {
   using std::swap;
@@ -172,14 +182,14 @@ struct btree_is_key_compare_to
     : public std::is_convertible<Compare, btree_key_compare_to_tag> {
 };
 
-// A helper class to convert a boolean comparison into a three-way "compare-to"
-// comparison that returns a negative value to indicate less-than, zero to
-// indicate equality and a positive value to indicate greater-than. This helper
-// class is specialized for less<string>, greater<string>, less<StringPiece>,
-// greater<StringPiece>, less<Cord> and greater<Cord>. The
-// btree_key_compare_to_adapter class is provided so that btree users
-// automatically get the more efficient compare-to code when using common
-// google string types with common comparison functors.
+// A helper class to convert a boolean comparison into a three-way
+// "compare-to" comparison that returns a negative value to indicate
+// less-than, zero to indicate equality and a positive value to
+// indicate greater-than. This helper class is specialized for
+// less<string> and greater<string>. The btree_key_compare_to_adapter
+// class is provided so that btree users automatically get the more
+// efficient compare-to code when using common google string types
+// with common comparison functors.
 template <typename Compare>
 struct btree_key_compare_to_adapter : Compare {
   btree_key_compare_to_adapter() { }
